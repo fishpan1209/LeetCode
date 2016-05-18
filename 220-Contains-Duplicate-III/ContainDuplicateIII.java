a4859e3cf082cf9ddfbc7e33a33e46bf3a18843f
fishpan1209 <fishpan1209@gmail.com>
2016-05-18 16:06:19 -0400

    [220. Contains Duplicate III][Accepted]committed by LeetCode Extension

--- 220-Contains-Duplicate-III/ContainDuplicateIII.java
+++ 220-Contains-Duplicate-III/ContainDuplicateIII.java
@@ -1,9 +1,21 @@
+import java.util.TreeSet;
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if(nums==null || nums.length<1 || k>nums.length) return false;
-        for(int i=0; i<nums.length-k; i++){
-            for(int j=i+1; j<=i+k; j++){
-                if(Math.abs(nums[j]-nums[i])<=t) return true;
+        if(nums==null || nums.length<1) return false;
+        final TreeSet<Long> values = new TreeSet<>();
+        for(int i=0; i<nums.length; i++){
+            // if nums[i]=Integer.MAX_VALUE, automatically handles overflow
+            // type must be Long, not long!
+            // TreeSet.floor(): get the floor value within the range [nums[i], nums[i]+t]
+            Long floor = values.floor((long) nums[i]+t);
+            // nums[i]-floor<=t, true
+            if(floor != null && floor + t >= nums[i]){
+                return true;
+            }
+            
+            values.add((long) nums[i]);
+            if(i>=k){
+                values.remove((long) nums[i-k]);
             }
         }
         return false;
