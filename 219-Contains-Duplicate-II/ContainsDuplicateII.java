02b2b8be4c7957d664559a532753ff7a4e28653d
fishpan1209 <fishpan1209@gmail.com>
2016-05-18 14:42:19 -0400

    [219. Contains Duplicate II][Accepted]committed by LeetCode Extension

--- 219-Contains-Duplicate-II/ContainsDuplicateII.java
+++ 219-Contains-Duplicate-II/ContainsDuplicateII.java
@@ -5,7 +5,7 @@ public class Solution {
         Hashtable<Integer, Integer> ht = new Hashtable();
         for(int i=0; i<nums.length; i++){
             if(ht.containsKey(nums[i])){
-                if((i-ht.get(nums[i]))==k) return true;
+                if((i-ht.get(nums[i]))<=k) return true;
             }
             ht.put(nums[i],i);
         }
