60d923988f1890062b8b006504783796e8e025f0
fishpan1209 <fishpan1209@gmail.com>
2016-05-18 14:40:58 -0400

    [219. Contains Duplicate II][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 219-Contains-Duplicate-II/ContainsDuplicateII.java
@@ -0,0 +1,14 @@
+import java.util.Hashtable;
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(nums==null || nums.length<1) return false;
+        Hashtable<Integer, Integer> ht = new Hashtable();
+        for(int i=0; i<nums.length; i++){
+            if(ht.containsKey(nums[i])){
+                if((i-ht.get(nums[i]))==k) return true;
+            }
+            ht.put(nums[i],i);
+        }
+        return false;
+    }
+}
