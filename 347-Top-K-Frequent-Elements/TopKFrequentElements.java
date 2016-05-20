41a0f6875ad793499fe378153272edce9a296cf3
fishpan1209 <fishpan1209@gmail.com>
2016-05-20 14:22:18 -0400

    [347. Top K Frequent Elements][Accepted]committed by LeetCode Extension

--- 347-Top-K-Frequent-Elements/TopKFrequentElements.java
+++ 347-Top-K-Frequent-Elements/TopKFrequentElements.java
@@ -3,7 +3,7 @@ public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
         List<Integer> res = new ArrayList();
         HashMap<Integer, Integer> map = new HashMap();
-        List<Integer>[] bucket = new List[nums.length];
+        List<Integer>[] bucket = new List[nums.length+1];
         for(int i=0; i<nums.length; i++){
             if(map.containsKey(nums[i])) {
                 map.put(nums[i],map.get(nums[i])+1);
