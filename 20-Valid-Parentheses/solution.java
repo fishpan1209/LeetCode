4533048e5c2df0372cd89d3062c7c70f62e809ca
fishpan1209 <fishpan1209@gmail.com>
2016-06-06 02:13:43 -0400

    [20. Valid Parentheses][Accepted]committed by LeetCode Extension

---
+++ 20-Valid-Parentheses/solution.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+public class Solution {
+    public boolean isValid(String s) {
+        if(s==null||s.length()%2==1)
+            return false;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='['||c=='{'||c=='(')
+                stack.push(c);
+            else{
+                if(stack.isEmpty())
+                    return false;
+                else if(c==']' && stack.pop()!='[')
+                    return false;
+                else if(c==')' && stack.pop()!='(')
+                    return false;
+                else if(c=='}' && stack.pop()!='{')
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
