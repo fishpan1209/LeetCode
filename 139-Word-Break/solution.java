08cc0088f0f293e68aacca66de0c18cc958c09b7
fishpan1209 <fishpan1209@gmail.com>
2016-06-10 12:54:10 -0400

    [139. Word Break][Accepted]committed by LeetCode Extension

---
+++ 139-Word-Break/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        int n = s.length();
+        if(s==null || n==0) return false;
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+        // right boundary must reach n because substring doesn't include right boundary
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                if(dp[j] && wordDict.contains(s.substring(j,i))) {
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+}
