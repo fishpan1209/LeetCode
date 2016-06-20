01f7e1e04410c4f2acd0c03ec63ca78f06f483f5
fishpan1209 <fishpan1209@gmail.com>
2016-06-20 19:01:55 -0400

    [14. Longest Common Prefix][Accepted]committed by LeetCode Extension

--- 14-Longest-Common-Prefix/solution.java
+++ 14-Longest-Common-Prefix/solution.java
@@ -3,6 +3,7 @@ public class Solution {
         if(strs==null || strs.length==0) return "";
         if(strs.length==1) return strs[0];
         // vertical scanning, O(S), O(1)
+        /*
         // find minlen string first
         int minLen = strs[0].length();
         String minS = strs[0];
@@ -20,5 +21,28 @@ public class Solution {
             }
         }
         return minS;
+        */
+        
+        // divide and conquer
+        return LCP(strs, 0, strs.length-1);
+    }
+    
+    public String LCP(String[] strs, int start, int end){
+        if(start==end) return strs[start];
+        else {
+            int mid = start+(end-start)/2;
+            String left = LCP(strs, start, mid);
+            String right = LCP(strs, mid+1, end);
+            return commonPrefix(left, right);
+        }
+
+    }
+    
+    public String commonPrefix(String left, String right){
+        int min = Math.min(left.length(), right.length());
+        for(int i=0; i<min; i++){
+            if(left.charAt(i)!=right.charAt(i)) return left.substring(0, i);
+        }
+        return left.substring(0,min);
     }
 }
