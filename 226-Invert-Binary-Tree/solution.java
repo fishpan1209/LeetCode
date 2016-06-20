bd123a48471235a33c137ff0893171ca44379c7a
fishpan1209 <fishpan1209@gmail.com>
2016-06-20 19:23:03 -0400

    [226. Invert Binary Tree][Accepted]committed by LeetCode Extension

--- 226-Invert-Binary-Tree/solution.java
+++ 226-Invert-Binary-Tree/solution.java
@@ -10,11 +10,28 @@
 public class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root==null) return root;
+        // recursive, O(n), O(h)
+        /*
         TreeNode left = invertTree(root.left);
         TreeNode right = invertTree(root.right);
         root.left = right;
         root.right = left;
         
         return root;
+        */
+        
+        // iterative
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode cur = queue.poll();
+            TreeNode temp = cur.left;
+            cur.left = cur.right;
+            cur.right = temp;
+            if(cur.left!=null) queue.add(cur.left);
+            if(cur.right!=null) queue.add(cur.right);
+        }
+        return root;
+        
     }
 }
