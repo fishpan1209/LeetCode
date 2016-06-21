9196eac39a8432c94ef61d273049dc1ea44a93bc
fishpan1209 <fishpan1209@gmail.com>
2016-06-21 01:18:18 -0400

    [242. Valid Anagram][Accepted]committed by LeetCode Extension

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        int[] count = new int[26];
+        int[] ch = new int[26];
         for(int i=0; i<s.length(); i++){
-            count[s.charAt(i)-'a']++;
+            ch[s.charAt(i)-'a']++;
         }
-        for(int i=0; i<t.length(); i++){
-            count[t.charAt(i)-'a']--;
+        for(int j=0; j<t.length(); j++){
+            ch[t.charAt(j)-'a']--;
         }
-        for(int i=0; i<count.length; i++){
-            if(count[i]!=0) return false;
+        for(int k=0; k<ch.length; k++){
+            if(ch[k]!=0) return false;
         }
         return true;
     }
