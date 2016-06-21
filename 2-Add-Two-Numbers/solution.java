a1ce5fa27126b609e3f4d10cee7ae05448f0f889
fishpan1209 <fishpan1209@gmail.com>
2016-06-21 15:33:13 -0400

    [2. Add Two Numbers][Accepted]committed by LeetCode Extension

--- 2-Add-Two-Numbers/solution.java
+++ 2-Add-Two-Numbers/solution.java
@@ -8,36 +8,25 @@
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(l1==null && l2==null) return null;
-        else if(l1==null) return l2;
-        else if(l2==null) return l1;
-        ListNode res = new ListNode(0);
-        ListNode head = res; // have to record head of res, otherwise res will only be last node
+        if(l1==null) return l2;
+        if(l2==null) return l1;
         int carry = 0;
+        ListNode res = new ListNode(0);
+        ListNode dummy = res;
+
         while(l1!=null || l2!=null){
-            
-            if(l1!=null) {
-                res.val+=l1.val;
-                l1 = l1.next==null? null : l1.next;
-            }
-            if(l2!=null) {
-                res.val+=l2.val;
-                l2 = l2.next==null? null : l2.next;
-            }
-            
-            res.val += carry;
-            carry = res.val>=10? 1 : 0;
-            
-            res.val %= 10;
-            
-            
-            if (l1!=null || l2!=null){
-                res.next = new ListNode(0);
-                res = res.next;
-            }
-            
+            int val1 = l1==null? 0 : l1.val;
+            int val2 = l2==null ? 0: l2.val;
+            int sum = val1+val2+carry;
+            carry = sum>=10 ? 1 : 0;
+            sum %= 10;
+            res.next = new ListNode(sum);
+            res = res.next;
+            if(l1!=null) l1 = l1.next;
+            if(l2!=null) l2 = l2.next;
         }
         if(carry>0) res.next = new ListNode(carry);
-        return head;
+        
+        return dummy.next;
     }
 }
