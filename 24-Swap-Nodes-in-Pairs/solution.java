10458cfbba69a4c47ba0cff92c37170ad0d0200b
fishpan1209 <fishpan1209@gmail.com>
2016-06-22 14:32:40 -0400

    [24. Swap Nodes in Pairs][Accepted]committed by LeetCode Extension

---
+++ 24-Swap-Nodes-in-Pairs/solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode res = dummy;
+        ListNode cur = dummy.next;
+        ListNode next = cur;
+        ListNode pre = cur;
+        while(cur!=null && cur.next!=null){
+            pre = cur;
+            next = cur.next;
+            cur = cur.next.next==null ? null : cur.next.next;
+            pre.next = cur;
+            dummy.next = next;
+            next.next = pre;
+            dummy = pre;
+        }
+        return res.next;
+    }
+}
