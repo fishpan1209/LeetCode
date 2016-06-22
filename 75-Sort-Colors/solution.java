ff11528b06c031acd28b5b36ca9283dff4620b50
fishpan1209 <fishpan1209@gmail.com>
2016-06-21 23:48:36 -0400

    [75. Sort Colors][Accepted]committed by LeetCode Extension

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public void sortColors(int[] nums) {
         // insertion sort, O(n2), O(1)
+        /*
         if(nums==null || nums.length<2) return;
         for(int i=1; i<nums.length; i++){
             for(int j=i; j>0; j--){
@@ -11,6 +12,26 @@ public class Solution {
                 }
             }
         }
+    }
+    */
+    
+    // put all 0s to left and 2s to right
+    int i=0;
+    int n = nums.length-1; 
+    for(int k=0; k<=n; k++){
+        while(nums[k]==2 && k<n) {
+            int t = nums[k];
+            nums[k] = nums[n];
+            nums[n] = t;
+            n--;
+        }
+        while(nums[k]==0 && k>i) {
+            int t = nums[k];
+            nums[k] = nums[i];
+            nums[i] = t;
+            i++;
+        }
+    }
     }
  
 }
