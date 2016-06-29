67cebe2abb44cdf349e6a19bfc86f71b45f514ef
fishpan1209 <fishpan1209@gmail.com>
2016-06-29 15:08:40 -0400

    [265. Paint House II][Accepted]committed by LeetCode Extension

--- 265-Paint-House-II/solution.java
+++ 265-Paint-House-II/solution.java
@@ -5,11 +5,16 @@ public class Solution {
         int k = costs[0].length;
         int[][] dp = new int[n][k];
         int min = Integer.MAX_VALUE;
+        // for each house, calculate the min cost of using each color
+        // for the last house, pick the min cost among all colors
         for(int i=0; i<n; i++){
+            // the first house
             if(i==0) dp[i] = costs[i];
             else{
+                // must save dp[i-1] first because it will be changed when calculating min
                 int[] cur = dp[i-1];
                 for(int j=0; j<k; j++){
+                    // calculate the min cost except color j
                     dp[i][j] = costs[i][j]+min(cur, j);
                 }
             }
