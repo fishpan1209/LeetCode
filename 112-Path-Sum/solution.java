dee6912bf20fa4a18425154f54b53cd7728c8a4c
fishpan1209 <fishpan1209@gmail.com>
2016-06-30 14:02:57 -0400

    [112. Path Sum][Accepted]committed by LeetCode Extension

--- 112-Path-Sum/solution.java
+++ 112-Path-Sum/solution.java
@@ -12,7 +12,7 @@ public class Solution {
         if(root==null) return false;
         if(root.left==null && root.right==null) return root.val==sum;
         if(root.left==null) return hasPathSum(root.right, sum-root.val);
-        if(root.right==null) return hasPathSum(root.left, sum-root.val);
+        if(root.right ==  null) return hasPathSum(root.left, sum-root.val);
         return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
     }
 }
