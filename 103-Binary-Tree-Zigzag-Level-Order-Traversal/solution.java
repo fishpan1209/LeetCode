3ef53506c32950954528ea48e189b78eb0d053de
fishpan1209 <fishpan1209@gmail.com>
2016-07-04 00:21:38 -0400

    [103. Binary Tree Zigzag Level Order Traversal][Accepted]committed by LeetCode Extension

--- 103-Binary-Tree-Zigzag-Level-Order-Traversal/solution.java
+++ 103-Binary-Tree-Zigzag-Level-Order-Traversal/solution.java
@@ -10,26 +10,24 @@
 public class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList();
-        if(root==null) return res;
         zigzag(root, res, 0);
         return res;
     }
     
-    public void zigzag(TreeNode root, List<List<Integer>> res,  int level){
+    public void zigzag(TreeNode root, List<List<Integer>> res, int level){
         if(root==null) return;
         if(res.size()<=level){
-            ArrayList cur = new ArrayList();
+            List<Integer> cur = new ArrayList();
             res.add(cur);
         }
-        List<Integer> temp = res.get(level);
-    
-        if(level>0 && level%2==0){
-            temp.add(root.val);
+        List<Integer> cur = res.get(level);
+        
+        if(level%2==0){
+            cur.add(root.val);
         }
-        else{
-           temp.add(0, root.val);
-        }
+        else cur.add(0, root.val);
         zigzag(root.left, res, level+1);
         zigzag(root.right, res, level+1);
+       
     }
 }
