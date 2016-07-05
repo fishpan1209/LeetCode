b93b56d65409814d3f055a4977fbf441d98393c4
fishpan1209 <fishpan1209@gmail.com>
2016-07-05 16:06:34 -0400

    [199. Binary Tree Right Side View][Accepted]committed by LeetCode Extension

--- 199-Binary-Tree-Right-Side-View/solution.java
+++ 199-Binary-Tree-Right-Side-View/solution.java
@@ -10,19 +10,14 @@
 public class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList();
-        if(root==null) return res;
-        return rightSideView(root, res, 0);
+        rightSideView(root, res, 0);
+        return res;
     }
     
-    public List<Integer> rightSideView(TreeNode root, List<Integer> res, int level){
-        if(root==null){
-            return res;
-        }
-        if(res.size()==level){
-            res.add(root.val);
-        }
+    public void rightSideView(TreeNode root, List<Integer> res, int level){
+        if(root==null) return;
+        if(res.size()==level) res.add(root.val);
         rightSideView(root.right, res, level+1);
         rightSideView(root.left, res, level+1);
-        return res;
     }
 }
