ee5e40c73594fa4088c58486abf63df135520bdc
fishpan1209 <fishpan1209@gmail.com>
2016-07-09 01:21:34 -0400

    [234. Palindrome Linked List][Accepted]committed by LeetCode Extension

--- 234-Palindrome-Linked-List/solution.java
+++ 234-Palindrome-Linked-List/solution.java
@@ -9,27 +9,30 @@
 public class Solution {
     public boolean isPalindrome(ListNode head) {
         if(head==null || head.next==null) return true;
-        if(head.next.next==null) return head.val==head.next.val;
-        ListNode slow=head, fast=head;
-        ListNode pre = slow, after = slow.next;
-        while(fast.next!=null && fast.next.next!=null){
-            fast = fast.next.next;
-            pre = slow;
-            slow = after; 
-            after = after.next;
-            slow.next = pre;
-        }
-       
-        // odd length
-        if(fast.next==null) slow = slow.next;
-       
-        // slow can be the last node, so slow!=null instead of slow.next!=null
-        while(after!=null){
-            if(slow.val!=after.val) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
             slow = slow.next;
-            after = after.next;
+            fast = fast.next.next==null? null : fast.next.next;
         }
         
+        // reverse first half
+        ListNode p1=head;
+        ListNode p2=head.next;
+        head.next=null;
+        while(p2!=slow){
+            ListNode t = p2.next;
+            p2.next = p1;
+            p1 = p2;
+            p2 = t;
+        }
+        // odd length
+        if(fast!=null) slow = slow.next;
+        while(slow!=null){
+            if(p1.val!=slow.val) return false;
+            p1 = p1.next;
+            slow = slow.next;
+        }
         return true;
     }
 }
