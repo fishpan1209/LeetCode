12f123caa7a7d608c933419fd1f192e24c874dc0
fishpan1209 <fishpan1209@gmail.com>
2016-07-13 01:11:11 -0400

    [97. Interleaving String][Compile Error]committed by LeetCode Extension

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -1,13 +1,29 @@
 public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if((s1.length()+s2.length())!=s3.length()) return false;
-        return interleave(s1, 0, s2, 0, s3, 0);
-    }
-    
-    public boolean interleave(String s1, int p1, String s2, int p2, String s3, int p3){
-        
-        if(p1==s1.length()) return s2.substring(p2).equals(s3.substring(p3));
-        if(p2==s2.length()) return s1.substring(p1).equals(s3.substring(p3));
-        return (s1.charAt(p1)==s3.charAt(p3) && interleave(s1, p1+1, s2, p2, s3, p3+1)) || (s2.charAt(p2)==s3.charAt(p3) && interleave(s1, p1, s2, p2+1, s3, p3+1));
-    }
+        private static Set<Integer> visited; // The combination of i1, i2 has been visited and return false
+	public static boolean isInterleave(String s1, String s2, String s3) {
+		if(s3.length() != s1.length() + s2.length())
+			return false;
+		visited = new HashSet<Integer>();
+		return isInterleave(s1, 0, s2, 0, s3, 0);
+	}
+	
+	private static boolean isInterleave(String s1, int i1, String s2, int i2, String s3, int i3)
+	{	
+		int hash = i1 * s3.length() + i2;
+		if(visited.contains(hash))
+			return false;
+		
+		if(i1 == s1.length())
+			return s2.substring(i2).equals(s3.substring(i3));
+		if(i2 == s2.length())
+			return s1.substring(i1).equals(s3.substring(i3));
+		
+		if(s3.charAt(i3) == s1.charAt(i1) && isInterleave(s1, i1+1, s2, i2, s3, i3+1) ||
+		   s3.charAt(i3) == s2.charAt(i2) && isInterleave(s1, i1, s2, i2+1, s3, i3+1))
+			return true;
+		
+		visited.add(hash);
+		return false;
+	}
 }
