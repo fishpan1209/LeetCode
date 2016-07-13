612b6042576792cefaed4849fbcbce712131bdcc
fishpan1209 <fishpan1209@gmail.com>
2016-07-13 01:09:19 -0400

    [97. Interleaving String][Time Limit Exceeded]committed by LeetCode Extension

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -8,6 +8,6 @@ public class Solution {
         
         if(p1==s1.length()) return s2.substring(p2).equals(s3.substring(p3));
         if(p2==s2.length()) return s1.substring(p1).equals(s3.substring(p3));
-        return (s1.charAt(p1)==s3.charAt(p3++) && interleave(s1, p1+1, s2, p2, s3, p3)) || (s2.charAt(p2)==s3.charAt(p3++) && interleave(s1, p1, s2, p2+1, s3, p3));
+        return (s1.charAt(p1)==s3.charAt(p3) && interleave(s1, p1+1, s2, p2, s3, p3+1)) || (s2.charAt(p2)==s3.charAt(p3) && interleave(s1, p1, s2, p2+1, s3, p3+1));
     }
 }
