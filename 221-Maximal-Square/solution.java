4e277a19fdac0b4c4538fc0e6331f4adcdc81d6c
fishpan1209 <fishpan1209@gmail.com>
2016-07-14 00:34:54 -0400

    [221. Maximal Square][Accepted]committed by LeetCode Extension

--- 221-Maximal-Square/solution.java
+++ 221-Maximal-Square/solution.java
@@ -8,7 +8,7 @@ public class Solution {
         
         for(int i=1; i<=m; i++){
             for(int j=1; j<=n; j++){
-                if(matrix[i][j]=='1'){
+                if(matrix[i-1][j-1]=='1'){
                     // dp[i][j] is at the lower right corner of formed square, dp is the side len of the square
                     // if matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1] all equals 1, then a square is formed, side length +=1
                     dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]),dp[i-1][j-1])+1;
