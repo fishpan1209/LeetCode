a092073778384f78f2fab6240a8987e0e1f79a43
fishpan1209 <fishpan1209@gmail.com>
2016-07-31 01:14:24 -0400

    [53. Maximum Subarray][Time Limit Exceeded]committed by LeetCode Extension

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,4 +1,6 @@
 public class Solution {
+    /*
+    // dp solution
     public int maxSubArray(int[] nums) {
         if(nums==null || nums.length==0) return 0;
         int len = nums.length;
@@ -9,5 +11,37 @@ public class Solution {
             res = Math.max(res, maxCur);
         }
         return res;
+    }
+    */
+    
+    // divide and conqure
+    public int maxSubArray(int[] nums){
+        if(nums==null || nums.length==0) return 0;
+        int n = nums.length;
+        return subarray(nums, 0, n-1);
+    }
+    
+    public int subarray(int[] nums, int start, int end){
+        if(start==end) return nums[start];
+        int mid = (start+end)/2;
+        int left = subarray(nums, start, mid);
+        int right = subarray(nums, mid+1, end);
+        
+        int leftMax = nums[mid];
+        int rightMax = nums[mid+1];
+        
+        int leftCur = leftMax;
+        for(int i=mid-1; i>=0; i--){
+            leftCur += nums[i];
+            leftMax = Math.max(leftMax, leftCur);
+        }
+        
+        int rightCur = rightMax;
+        for(int j=mid+2; j<=end; j++){
+            rightCur += nums[j];
+            rightMax = Math.max(rightMax, rightCur);
+        }
+        
+        return Math.max(Math.max(left, right), leftMax+rightMax);
     }
 }
