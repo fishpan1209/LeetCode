84856dc356225267c6904401fab1a3d53da79f0b
fishpan1209 <fishpan1209@gmail.com>
2016-07-30 23:55:35 -0400

    [53. Maximum Subarray][Accepted]committed by LeetCode Extension

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,12 +1,13 @@
 public class Solution {
     public int maxSubArray(int[] nums) {
         if(nums==null || nums.length==0) return 0;
+        int len = nums.length;
         int maxCur = nums[0];
-        int maxSofar = nums[0];
-        for(int i=1; i<nums.length; i++){
-            maxCur = Math.max(maxCur+nums[i], nums[i]);
-            maxSofar = Math.max(maxSofar, maxCur);
+        int res = maxCur;
+        for(int i=1; i<len; i++){
+            maxCur = Math.max(nums[i], nums[i]+maxCur);
+            res = Math.max(res, maxCur);
         }
-        return maxSofar;
+        return res;
     }
 }
