1fbc1ffe37c8de47f945f0b456649830a399e373
fishpan1209 <fishpan1209@gmail.com>
2016-08-04 22:42:56 -0400

    [139. Word Break][Accepted]committed by LeetCode Extension

--- 139-Word-Break/solution0804.java
+++ 139-Word-Break/solution0804.java
@@ -1,20 +1,23 @@
 public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
+        // if empty set, return false
         if(wordDict==null || wordDict.size()==0) return false;
+        // if empty string, return true
         if(s==null || s.length()==0) return true;
-        if(s.length()==1) return wordDict.contains(s);
         
-            for(int j=1; j<=s.length(); j++){
-                String str = s.substring(0, j);
-                System.out.println("str: "+str);
-                if(wordDict.contains(str)){
-                    String rest = s.substring(j, s.length());
-                    System.out.println("str: "+str+" rest: "+rest);
-                    if(wordBreak(rest, wordDict)) return true;
-            
-                }
-                
+        // dp
+        int n = s.length();
+        boolean dp[] = new boolean[n+1];
+        // empty string
+        dp[0] = true;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                dp[i] = (wordDict.contains(s.substring(j, i)) && dp[j]);
+                // break whenever found a pair
+                if(dp[i]) break;
             }
-        return false;
+        }
+        return dp[n];
     }
 }
