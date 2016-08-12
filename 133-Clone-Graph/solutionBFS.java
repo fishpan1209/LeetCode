c51aba0648f5f2527b583c22a2d65b727826a031
fishpan1209 <fishpan1209@gmail.com>
2016-08-12 16:35:59 -0400

    [133. Clone Graph][Compile Error]committed by LeetCode Extension

---
+++ 133-Clone-Graph/solutionBFS.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    
+    // BFS
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node){
+        if(node == null) return node;
+        // use a map to avoid duplicate
+        HashMap<Integer, UndirectedGraphNode> map = new HashMap();
+        UndirectedGraphNode res = new UndirectedGraphNode(node.label);
+        
+        map.put(res.label, res);
+        
+        // create a queue to store all nodes
+        LinkedList<UndirectedGraphNode> queue = new LinkedList();
+        queue.add(node);
+        
+        while(!queue.isEmpty()){
+            UndirectedGraphNode current = queue.poll();
+            for(UndirectedGraphNode neighbor : current.neighbors){
+                // if not visited, add to map and queue
+                if(!map.containsKey(neighbor.label)){
+                    map.put(neighbor.lable, neighbor);
+                    queue.add(neighbor);
+                }
+                // else add this node as a neighbor of current node
+                map.get(current.label).neighbors.add(map.get(neighbor.label));
+            }
+        }
+        return res;
+    }
+    /*
+    // DFS
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        // use a map to store all unique nodes
+        HashMap<Integer, UndirectedGraphNode> map = new HashMap();
+        return clone(node, map);
+    }
+    
+    public UndirectedGraphNode clone(UndirectedGraphNode node, HashMap<Integer, UndirectedGraphNode> map){
+        if(node==null) return node;
+        // this node has already been visited as a neighbor
+        if(map.containsKey(node.label)){
+            return map.get(node.label);
+        }
+        // create new clone
+        UndirectedGraphNode res = new UndirectedGraphNode(node.label);
+        map.put(res.label, res);
+        for(UndirectedGraphNode neighbor : node.neighbors){
+            res.neighbors.add(clone(neighbor, map));
+        }
+        return res;
+    }
+    */
+}
