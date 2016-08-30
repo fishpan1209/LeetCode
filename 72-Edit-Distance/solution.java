df9d808a073bcd2fad992b22d907f1cc1aca596a
fishpan1209 <fishpan1209@gmail.com>
2016-08-29 20:23:46 -0400

    [72. Edit Distance][Accepted]committed by LeetCode Extension

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -20,7 +20,7 @@ public class Solution {
         
         for(int i=1; i<=len1; i++){
             for(int j=1; j<=len2; j++){
-                if(word1.charAt(i-1) == word2.charAt(i-1)){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
                     distance[i][j] = distance[i-1][j-1];
                 }
                 else{
